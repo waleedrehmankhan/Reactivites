e583e62e3e4cd3418b2724f0af82a361faa69c0f
Waleed Rehman Khan <waleedrehmankhan@outlook.com>
2021-03-29 12:06:38 +1100

    End of Section 3 React Skeleton

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
+
+  const [activitites, setactivities] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities').then(response => {
+      setactivities(response.data);
+      console.log(response);
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header as='h2' icon='users' content='Reactivities' />
+        <List>
+          {activitites.map((activity: any)=> (
+            <List.Item key= {activity.id}>
+              {activity.title}
+            </List.Item>
+          ))}
+        </List>
     </div>
   );
 }
