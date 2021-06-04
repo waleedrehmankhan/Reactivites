9c99ad5171d4154773d2c7615e9739eb32ee0c67
Waleed Rehman Khan <waleedrehmankhan@outlook.com>
2021-06-04 15:25:18 +1000

    End of Form Implementation

---
+++ client-app/src/app/common/form/MyTextInput.tsx
@@ -0,0 +1,22 @@
+import { useField } from 'formik';
+import React from 'react';
+import { Form, Label } from 'semantic-ui-react';
+
+interface Props {
+    placeholder: string,
+    name: string,
+    label?: string
+}
+
+export default function MyTextInput(props: Props) {
+    const [Field, meta] = useField(props.name);
+    return (
+        <Form.Field error={meta.touched && !!meta.error}>
+            <label>{props.label}</label>
+            <input {...Field} {...props} />
+            {meta.touched && meta.error ? (
+                <Label basic color='red'>{meta.error}</Label>
+            ) : null}
+        </Form.Field>
+    )
+}
